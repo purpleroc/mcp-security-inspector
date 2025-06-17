e9be2ca721a5da484e78384d0644fb91339084e2
purpleroc <purpleroc>
2025-06-17 16:58:04 +0800

    尝试使用 webRequest 来解决 cors的问题

--- public/background.js
+++ public/background.js
@@ -9,4 +9,36 @@ chrome.action.onClicked.addListener((tab) => {
 // 监听扩展安装事件
 chrome.runtime.onInstalled.addListener(() => {
   console.log('MCP Security Inspector 已安装');
-}); 
+});
+
+// 处理CORS - 修改响应头
+chrome.webRequest.onHeadersReceived.addListener(
+  function(details) {
+    const headers = details.responseHeaders || [];
+    
+    // 添加CORS响应头
+    headers.push({
+      name: 'Access-Control-Allow-Origin',
+      value: '*'
+    });
+    headers.push({
+      name: 'Access-Control-Allow-Methods',
+      value: 'GET, POST, PUT, DELETE, OPTIONS'
+    });
+    headers.push({
+      name: 'Access-Control-Allow-Headers',
+      value: 'Content-Type, Authorization, X-Requested-With'
+    });
+    headers.push({
+      name: 'Access-Control-Allow-Credentials',
+      value: 'true'
+    });
+
+    return { responseHeaders: headers };
+  },
+  {
+    urls: ["<all_urls>"],
+    types: ["xmlhttprequest", "other"]
+  },
+  ["blocking", "responseHeaders", "extraHeaders"]
+); 
